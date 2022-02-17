bd1feb45aed65503c2173e1de77e93431e604321
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2022-02-17 12:07:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from glob import glob
 from setuptools import setup
 
-package_name = "assignment3-gazebo_simulation"
+package_name = "assignment3-gazebo_simulation-user"
 
 setup(
     name=package_name,
